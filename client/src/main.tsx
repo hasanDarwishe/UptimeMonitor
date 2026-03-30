2cb0299765ed047271fb2a2ff0dc642febc65eb9
Hasan Darwish <zceline76@gmail.com>
2026-03-30 13:25:24 +0300

    Integrated the Main font and deleted the unnecessary files (license, README, and static font files) of the fonts

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,8 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css'
+
 import '../node_modules/bootstrap-icons/font/bootstrap-icons.min.css';
+import './fonts.css';
+import './index.css';
+
+
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
