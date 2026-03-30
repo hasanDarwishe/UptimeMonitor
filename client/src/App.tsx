227c1d12433735bcbf5127f52bcc93e5dcd66b17
Hasan Darwish <zceline76@gmail.com>
2026-03-30 22:58:56 +0300

    Added the navigation bar to the main page and added the routings to the application

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,32 @@
 import './App.css';
-import { Router, Routes } from 'react-router-dom';
+import { Route, Routes, NavLink, useLocation } from 'react-router-dom';
+
+import { Home } from './pages/Home';
+
+function NavigationButton({icon, text, link}:{icon: string, text: string, link: string}) {
+  const location = useLocation();
+  const currentPathname = location.pathname;
+  const active = currentPathname === link;
+
+  return <NavLink to={link} className={`navigationButton ${active && "active"}`}>
+    <i className={`bi bi-${icon}`}></i>
+    <div className="text">{text}</div>
+  </NavLink>
+}
 
 function App() {
   return (
-    <>
+    <div style={{paddingTop: 50}}>
+      {/* Navigation Bar */}
+      <header className="navigationBar">
+        <NavigationButton link='/' icon='house' text='Home'/>
+      </header>
+
       {/* Routes */}
       <Routes>
+        <Route path='/' element={<Home />}></Route>
       </Routes>
-    </>
+    </div>
   );
 }
 
